0f5085f5cd5c0f1a852b817a8643822150781d97
Gregor Parzefall <gregor.parzefall@posteo.de>
2023-03-31 19:41:07 +0200

    Stuff that was still in my working directory when I restored the repo
    Zuletzt geändert: 11. Februar 2022

--- init.lua
+++ init.lua
@@ -79,3 +79,59 @@ function GrenadeSuck.on_trigger(position)
 end
 
 grenade.register(GrenadeSuck)
+
+function darkness(pos)
+    local pos_min = pos - vector.new(11, 11, 11)
+    local pos_max = pos + vector.new(11, 11, 11)
+
+    local vmanip = minetest.get_voxel_manip()
+
+    vmanip:read_from_map(pos_min, pos_max)
+    local vmanip_pos_min, vmanip_pos_max = vmanip:get_emerged_area()
+    local vmanip_area = VoxelArea:new({MinEdge = vmanip_pos_min, MaxEdge = vmanip_pos_max})
+
+    local light_data = vmanip:get_light_data()
+
+    for ring = 0, 11 do
+        for x = pos.x - ring, pos.x + ring do
+            for y = pos.y - ring, pos.y + ring do
+                for z = pos.z - ring, pos.z + ring do
+                    if (
+                        x == pos.x - ring or x == pos.x + ring or
+                        y == pos.y - ring or y == pos.y + ring or
+                        z == pos.z - ring or z == pos.z + ring
+                    ) then
+                        local index = vmanip_area:index(x, y, z)
+
+                        local light_old = light_data[index] % 16
+                        local light_new = (
+                            ring <= 7 and 0 or
+                            (ring - 7) * 3
+                        )
+                        if light_new > light_old then
+                            light_new = light_old
+                        end
+                        light_data[index] = light_new
+                    end
+                end
+            end
+        end
+    end
+
+    vmanip:set_light_data(light_data)
+    vmanip:write_to_map(false)
+end
+
+local GrenadeDarkness = {
+	name = "grenade:grenade_darkness",
+	description = t("Darkness Grenade"),
+	texture = "grenade_darkness.png",
+}
+
+function GrenadeDarkness.on_trigger(pos)
+	local pos_rounded = pos:round()
+	minetest.sound_play("grenade_darkness_powder", {pos = pos_rounded}, true)
+	darkness(pos_rounded)
+end
+
+grenade.register(GrenadeDarkness)
